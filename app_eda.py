8da601174b3dd8888fbc4d4bedf9ab91e4a82799
leejiheon26 <ts24071@ts.hs.kr>
2025-06-17 04:37:49 +0000

    1

--- app_eda.py
+++ app_eda.py
@@ -203,9 +203,9 @@ class Logout:
 class EDA:
     def __init__(self):
         st.title("📊 Bike Sharing Demand EDA")
-        uploaded = st.file_uploader("데이터셋 업로드 (train.csv)", type="csv")
+        uploaded = st.file_uploader("데이터셋 업로드 (population.csv)", type="csv")
         if not uploaded:
-            st.info("train.csv 파일을 업로드 해주세요.")
+            st.info("population.csv 파일을 업로드 해주세요.")
             return
 
         df = pd.read_csv(uploaded, parse_dates=['datetime'])
